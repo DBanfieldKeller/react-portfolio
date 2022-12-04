ed3d2f4c6fe652b04d00eccddb9260c9074c56f9
Goat <dpajbk@gmail.com>
2022-12-04 16:20:26 -0500

    basic header and nav bar skeleton added

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from './Navigation';
+
+// create header
+export default function Header() {
+
+    return(
+        <div>
+        <h1>Daniel Banfield-Keller</h1>
+        <Navigation />
+        </div>
+
+    )
+
+}
