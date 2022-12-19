d91c9560cfc9dde47c8008919816c07b75bc6702
Goat <dpajbk@gmail.com>
2022-12-19 12:06:37 -0500

    updated about me section (to be improved)

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,11 +4,9 @@ import React from "react";
 export default function AboutMe() {
     return(
         <div className="container">
-        <h3>About Me (Coming Soon)</h3>
+        <h3>About Me </h3>
         <img className="img-fluid" src="https://raw.githubusercontent.com/DBanfieldKeller/react-portfolio/main/src/assets/FT84fB3XEAId64O.jpg" alt="me"/>    
-        <section>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac tellus vehicula, dignissim velit consequat, varius nisi. Proin pulvinar metus consequat, egestas neque ut, mollis urna. Aenean placerat nunc sit amet nisi blandit, vitae gravida erat dignissim. Quisque iaculis lectus ut est maximus, tincidunt elementum magna vehicula. Suspendisse ut lectus felis. Nam porta, massa et maximus convallis, mi turpis volutpat nisl, vitae accumsan leo leo sed elit. Maecenas auctor molestie iaculis.
-
-        Vivamus mi mi, ullamcorper vitae fringilla eget, tempus non enim. Vestibulum vel ultrices enim, ultricies aliquam lorem. Vivamus aliquet vestibulum mauris dictum pellentesque. Fusce faucibus aliquet augue in viverra. Ut sodales finibus odio. Phasellus ullamcorper orci a eros dignissim rhoncus at ac diam. Maecenas varius turpis tortor, vel iaculis mi dapibus in. Nulla nec porttitor erat, tincidunt pretium quam. Ut posuere quis orci eget ullamcorper. Nulla facilisi. Pellentesque eu tortor quis lacus semper venenatis. Mauris lobortis dolor eget mi porta, vel viverra leo sollicitudin. Fusce iaculis et metus in rutrum. Morbi nec tristique magna, sit amet molestie risus. Ut in ante et elit egestas molestie a sit amet justo.</section>
+        <section>I am a Javascript Web Application developer with fullstack experience. I have built a number of frontend and backend applications that are on display in the projects section of this website.  I graduated from UC Berkeley in 2016 with a BA in Linguistics and Japanese. Since that time I have worked for and with many Japanese companies. I am fluent in Japanese and comfortable in both American and Japanese business cultures. </section>
         </div>
     )
 }
