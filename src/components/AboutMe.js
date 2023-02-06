979512923d143d287f265c75fe2c12da4bbd2789
Goat <dpajbk@gmail.com>
2023-02-06 14:27:23 -0500

    updated about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
         <div className="container">
         <h3>About Me </h3>
         <img className="img-fluid" src="https://raw.githubusercontent.com/DBanfieldKeller/react-portfolio/main/src/assets/FT84fB3XEAId64O.jpg" alt="me"/>    
-        <section>I am a Javascript Web Application developer with fullstack experience. I have built a number of frontend and backend applications that are on display in the projects section of this website.  I graduated from UC Berkeley in 2016 with a BA in Linguistics and Japanese. Since that time I have worked for and with many Japanese companies. I am fluent in Japanese and comfortable in both American and Japanese business cultures. </section>
+        <section>I am a Full stack web developer with a number of frontend and backend applications that are on display in the projects section of this website. I graduated from UC Berkeley in 2016 with a BA in Linguistics and Japanese. Since that time I have worked for and with many Japanese companies. I am fluent in Japanese and comfortable in both American and Japanese business cultures. </section>
         </div>
     )
 }
